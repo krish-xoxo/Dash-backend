afbb0ab8b38cefa6428dcaa147e8646c1bcc4235
Krish Joshi <krish@xoxoday.com>
2024-01-02 22:38:05 +0530

    updated changes

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import express  from "express";
+import express from "express";
 import mysql from "mysql";
 import cors from "cors";
 import session from "express-session";
@@ -7,26 +7,26 @@ import bodyParser from "body-parser";
 import jwt from "jsonwebtoken";
 
 
-const app = express();  
+const app = express();
 
 app.use(cors({
     origin: ["http://localhost:3000"],
-    methods: ["POST","GET"],
+    methods: ["POST", "GET"],
     credentials: true
 }));
 
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(bodyParser.json()); 
+app.use(bodyParser.json());
 
 app.use(session({
     secret: "secret",
     resave: false,
     saveUninitialized: false,
-    cookie:{
+    cookie: {
         secure: false,
-        maxAge: 1000*60*60*24
+        maxAge: 1000 * 60 * 60 * 24
     }
 }))
 //const salt = 10;
@@ -39,16 +39,16 @@ const db = mysql.createConnection({
 })
 
 
-const verifyUser = (req,res,next) => {
+const verifyUser = (req, res, next) => {
     const token = req.cookies.token;
-    if(!token){
-        return res.json({Message: "No token found. Please login to create a token"})
-    }else{
-        jwt.verify(token, "mera-jsonwebtoken-private-key", (err,decoded) => {
-            if(err){
+    if (!token) {
+        return res.json({ Message: "No token found. Please login to create a token" })
+    } else {
+        jwt.verify(token, "mera-jsonwebtoken-private-key", (err, decoded) => {
+            if (err) {
                 console.log(err);
-                return res.json({Message: "Error"})
-            }else{
+                return res.json({ Message: "Error" })
+            } else {
                 req.name = decoded.name;
                 req.email = decoded.email;
                 req.id = decoded.id;
@@ -58,94 +58,88 @@ const verifyUser = (req,res,next) => {
     }
 }
 
-
-db.connect(function(err) {
-    if(err){
+db.connect(function (err) {
+    if (err) {
         console.log(err);
     }
-    else{
-        console.log("COnnected");
+    else {
+        console.log("Connected");
     }
-})
-
-
-app.get('/',verifyUser, (req,res) => {
-    return res.json({Status: "Success", name: req.name,email: req.email})
 })
 
 //TO VALIDATE USING WEBTOKENS
-app.get('/',verifyUser, (req,res) => {
-    return res.json({Status: "Success", name: req.name,email: req.email})
+app.get('/', verifyUser, (req, res) => {
+    return res.json({ Status: "Success", name: req.name, email: req.email })
 })
 
 //TO ENTER NORMAL PASSWORD(WITHOUT HASHED)[VALIDATED]:
-app.post('/signup', (req,res) => {
-        const email_patt = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        const sql = "INSERT INTO users (`name`, `email`, `password`) VALUES (?,?,?)";
-        const values =[
-            req.body.name,
-            req.body.email,
-            req.body.password
-        ];
-        console.log(values);
-        const sqlnew = "INSERT INTO userprofile (`name`, `email`, `user_id`) VALUES (?,?,?)";
-        const Duplicate = 'SELECT * FROM users WHERE email = ?';
-        db.query(Duplicate, [values.email], (duplicateErr, duplicateData) => {
-            if (duplicateErr) {
-                console.log(duplicateErr);
-                console.log(res.json);
-                return res.json("Error");
-            }
-            else if(duplicateData && duplicateData.length > 0) {
-                return res.status("Account already created");
-            }
-            else {
-                db.query(sql, values, (err, data) => {
-                    if (err) {
-                        console.log(err);
-                        return res.json("Error");
+app.post('/signup', (req, res) => {
+    const email_patt = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const sql = "INSERT INTO users (`name`, `email`, `password`) VALUES (?,?,?)";
+    const values = [
+        req.body.name,
+        req.body.email,
+        req.body.password
+    ];
+    console.log(values);
+    const sqlnew = "INSERT INTO userprofile (`name`, `email`, `user_id`) VALUES (?,?,?)";
+    const Duplicate = 'SELECT * FROM users WHERE email = ?';
+    db.query(Duplicate, [values.email], (duplicateErr, duplicateData) => {
+        if (duplicateErr) {
+            console.log(duplicateErr);
+            console.log(res.json);
+            return res.json("Error");
+        }
+        else if (duplicateData && duplicateData.length > 0) {
+            return res.status("Account already created");
+        }
+        else {
+            db.query(sql, values, (err, data) => {
+                if (err) {
+                    console.log(err);
+                    return res.json("Error");
+                }
+                console.log(data);
+                const value_new = [
+                    req.body.name,
+                    req.body.email,
+                    data.insertId
+                ];
+                db.query(sqlnew, value_new, (e, data) => {
+                    if (e) {
+                        console.log(e);
                     }
-                    console.log(data);
-                    const value_new = [
-                        req.body.name,  
-                        req.body.email,
-                        data.insertId
-                    ];
-                    db.query(sqlnew, value_new, (e,data) => {
-                        if(e){
-                            console.log(e);
-                        }
-                        else{
-                            console.log(data);
-                        }
-                    })
-                    return res.json(data);
+                    else {
+                        console.log(data);
+                    }
                 })
-            }
-        })
+                return res.json(data);
+            })
+        }
+    })
 })
 
 //TO LOGIN USING NORMAL PASSWORD: 
-app.post('/login', (req,res) => {
+app.post('/login', (req, res) => {
     const email_patt = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     const sql = "SELECT * FROM users WHERE `email` = ? && `password`=?";
     const values = [req.body.email];
-    db.query(sql,[req.body.email, req.body.password ], (err,data) => {
-        if(!email_patt.test(values.email)){
-            if(err){
+    db.query(sql, [req.body.email, req.body.password], (err, data) => {
+        if (!email_patt.test(values.email)) {
+            if (err) {
                 console.log(err);
                 return res.json("Error");
             }
-            else if(data.length > 0){
-                const name = data[0].name; 
-                const email =  data[0].email;
+            else if (data.length > 0) {
+                const name = data[0].name;
+                const email = data[0].email;
                 const id = data[0].id;
-                const token = jwt.sign({name,email,id},"mera-jsonwebtoken-private-key",{expiresIn:'1d'});
-                res.cookie('token',token);
+                const token = jwt.sign({ name, email, id }, "mera-jsonwebtoken-private-key", { expiresIn: '1d' });
+                res.cookie('token', token);
                 //console.log(data);
-                return res.json({Status:"Success"})
+                return res.json({ Status: "Success" })
             }
-            else{
+            else {
                 console.log(res.json);
                 return res.json("Fail");
             }
@@ -153,14 +147,14 @@ app.post('/login', (req,res) => {
     })
 })
 
-app.get('/logout', (req,res) => {
+app.get('/logout', (req, res) => {
     res.clearCookie('token');
-    return res.json({Status:"Success"})
+    return res.json({ Status: "Success" })
 })
 
-app.post('/userprofile', verifyUser, (req,res) => {
+app.post('/userprofile', verifyUser, (req, res) => {
     const sql = "UPDATE userprofile SET name = ?, email = ? , age = ?, gender = ?, mobilenumber = ?, address = ?, pincode = ?, city = ?, state = ?, country = ? WHERE user_id = ?";
-    const values =[
+    const values = [
         req.body.name,
         req.body.email,
         req.body.age,
@@ -171,34 +165,34 @@ app.post('/userprofile', verifyUser, (req,res) => {
         req.body.city,
         req.body.state,
         req.body.country,
-        req.id, 
-        ];
-        console.log(values);
-        db.query(sql,values,(err,data) => {
-            if(err){
-                console.log(err);
-                return res.json("Error");
-            }
-            else{
-                const sql_new = "UPDATE users SET name = ?, email = ? WHERE id=?";
-                const value_new = [
-                    req.body.name,
-                    req.body.email,
-                    req.id,
-                ];
-                db.query(sql_new, value_new, (e,data) => {
-                    if(e){
-                        console.log(e);
-                    }
-                    else{
+        req.id,
+    ];
+    console.log(values);
+    db.query(sql, values, (err, data) => {
+        if (err) {
+            console.log(err);
+            return res.json("Error");
+        }
+        else {
+            const sql_new = "UPDATE users SET name = ?, email = ? WHERE id=?";
+            const value_new = [
+                req.body.name,
+                req.body.email,
+                req.id,
+            ];
+            db.query(sql_new, value_new, (e, data) => {
+                if (e) {
+                    console.log(e);
+                }
+                else {
                     //console.log(data);
                     return res.json(data);
-                    }
-                })
-            }
-        })
-}); 
+                }
+            })
+        }
+    })
+});
 
-app.listen(8081, () =>{
+app.listen(8081, () => {
     console.log("Listening");
 })
